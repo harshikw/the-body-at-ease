0657d37598d2bcd532ab5900502a8ff4d681cf76
harshikw <tntharshi@gmail.com>
2025-11-12 12:12:58 +0530

    Add files via upload

---
+++ TheBodyAtEase.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TheBodyAtEase() {
+  return (
+    <div>
+      <h1>The Body at Ease</h1>
+      <p>Deployment fix version (no useState import).</p>
+    </div>
+  )
+}
